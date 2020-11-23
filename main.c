a5b6f5a0ce6dc1d95268838b2d9b87fb046dc893
Kenan Pasanbegovic <kenan.pasanbegovic@gmail.com>
2020-11-23 19:39:27 +0100

    initial commit

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "Rectangle.h""
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	Rectangle_t rect;
+	
+	rect.a = 3;
+	rect.b = 4;
+	return 0;
+}
